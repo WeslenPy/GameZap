52e0109d4c00b63223986945dec37ed52a97f963
WeslenPy <weslenjhony@gmail.com>
2023-03-19 16:58:10 -0300

    modify model

--- app/database/models/user_model.py
+++ app/database/models/user_model.py
@@ -16,18 +16,28 @@ class User(db.Model):
 
     cpf = db.Column(db.String(12),unique=True,default=None,nullable=True)
 
+    active = db.Column(db.Boolean,default=True,nullable=False)
+    full_profile = db.Column(db.Boolean,default=False,nullable=False)
+    banned = db.Column(db.Boolean,default=False,nullable=False)
+
     created_at = db.Column(db.DateTime,default=datetime.now,nullable=False)
 
     recharge_children = db.relationship(
         "Recharge", back_populates="user_ship",
         cascade="all, delete",passive_deletes=True)
     
-    def __init__(self,chat_id,device,username,full_name,balance=0,cpf=None,created_at=datetime.now):
+    def __init__(self,chat_id,device,username,full_name,active=True,full_profile=False,
+                            banned=False,balance=0,cpf=None,created_at=datetime.now):
+        
         self.chat_id = chat_id
         self.device = device
         self.username = username
         self.full_name = full_name
         self.balance = balance
+        self.banned = banned
+        self.full_profile = full_profile
+        self.full_profile = full_profile
+        self.active = active
         self.cpf = cpf
         self.created_at = created_at()
 
